e015569cc6da1db3a1e38ff1c44b8f315adf8db5
heshi906 <1059163384@qq.com>
2023-10-22 09:09:26 +0800

    修bug

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def refresh():
     ports = json.load(f)
     f.close()
 
+
+ports={}
 try:
     f = open("ports.json", "r+")
     ports = json.load(f)
@@ -20,6 +22,7 @@ with open("ddos日志.txt", 'a+') as f:
     time_data1 = datetime.datetime.now().strftime("%y/%m/%d/%H:%M")
     f.write(time_data1 + "\n\n")
 f.close()
+
 
 while 1:
     choice = input("[1]显示所有目标\n[2]ddos指定题目\n[3]ddos指定端口\n[4]刷新\n[5]压制题目\n")
